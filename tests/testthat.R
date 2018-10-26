ff85a44d58b94f94d2d02ead51fc752cbc16413b
David Hugh-Jones <davidhughjones@gmail.com>
2018-10-26 07:26:15 +0100

    Add basic tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(latexdiffr)
+
+test_check("latexdiffr")
---
+++ tests/testthat/test_basic.R
@@ -0,0 +1,15 @@
+
+
+context("Basic tests")
+
+test_that("All 3 file types compile", {
+  files1 <- c("foo-prerendered.tex", "foo-rnw.Rnw", "foo-rmd.Rmd")
+  files2 <- c("bar-prerendered.tex", "bar-rnw.Rnw", "bar-rmd.Rmd")
+
+  for (f1 in files1) for (f2 in files2) {
+    expect_error(latexdiff(f1, f2, open = FALSE), regexp = NA,
+          label = sprintf("File 1: %s, file 2: %s", f1, f2))
+    expect_true(file.exists("diff.pdf"))
+    if (file.exists("diff.pdf")) file.remove("diff.pdf")
+  }
+})
