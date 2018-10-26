efcb2eb13687460dfaf4cf7b500910e1ad66d584
David Hugh-Jones <davidhughjones@gmail.com>
2018-10-26 07:27:35 +0100

    Don't clean original tex files; accept file extensions in any case.

--- R/latexdiff.R
+++ R/latexdiff.R
@@ -18,6 +18,10 @@
 #' or `.rnw`. Rmd files are processed by [rmarkdown::render()]. Rnw files
 #' are processed by [knitr::knit()].
 #'
+#' `latexdiff` is not perfect. Some changes will confuse it. In particular:
+#'
+#' * Changing the document title may cause failures.
+#'
 #' @return
 #' Invisible NULL.
 #'
@@ -32,9 +36,9 @@ latexdiff <- function (path1, path2, output = "diff", open = interactive(), clea
   paths <- c(path1, path2)
   tex_paths <- rep(NA_character_, 2)
   for (idx in 1:2) {
-    tex_paths[idx] <- if (grepl("\\.tex", paths[idx])) {
+    tex_paths[idx] <- if (grepl("\\.tex", tolower(paths[idx]))) {
             paths[idx]
-          } else if (grepl("\\.Rmd$", paths[idx])) {
+          } else if (grepl("\\.rmd$", tolower(paths[idx]))) {
             loadNamespace("rmarkdown")
             if (missing(output_format)) {
               doc_opts <- rmarkdown::default_output_format(paths[idx])$options
@@ -42,11 +46,12 @@ latexdiff <- function (path1, path2, output = "diff", open = interactive(), clea
               output_format <- do.call(rmarkdown::latex_document, doc_opts)
             }
             rmarkdown::render(paths[idx], output_format = output_format)
-          } else if (grepl("\\.Rnw$", paths[idx])) {
+          } else if (grepl("\\.rnw$", tolower(paths[idx]))) {
             loadNamespace("knitr")
             knitr::knit(paths[idx])
           } else {
-            stop("Unrecognized file extension for '", paths[idx], "'")
+            stop("Unrecognized file extension for '", paths[idx], "'.",
+                  "Must be one of '.Rmd', '.tex' or '.rnw'.")
           }
   }
 
@@ -60,12 +65,16 @@ latexdiff <- function (path1, path2, output = "diff", open = interactive(), clea
     tools::texi2pdf(diff_tex_path, clean = clean)
   }
 
-  if (clean) file.remove(tex_paths)
+  if (clean) {
+    file.remove(setdiff(tex_paths, paths))
+    file.remove(diff_tex_path)
+  }
 
   diff_pdf_path <- paste0(output, ".pdf")
   if (open) {
     auto_open(diff_pdf_path)
   }
+
   return(invisible(NULL))
 }
 
