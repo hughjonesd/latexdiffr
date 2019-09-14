0524390f2bf4673791a2b27b2fcec9304d800b03
David Hugh-Jones <davidhughjones@gmail.com>
2019-09-14 18:37:25 +0100

    Force tests to be run on appveyor

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -2,9 +2,6 @@
 
 context("Basic tests")
 
-skip_on_cran()
-which_cmd <- if (Sys.info()["sysname"] == "Windows") "where" else "which"
-skip_if(system2(which_cmd, "latexdiff") != 0)
 
 check_and_remove <- function (path) {
   expect_true(file.exists(path), label = sprintf("file.exists('%s')", path))
